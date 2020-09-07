d7d46f094a35201fe6c4aa52f0344fbdd427e2c7
Karolis K <46462840+k-karolis@users.noreply.github.com>
2020-09-07 21:41:57 +0100

    Updates

---
+++ precache-manifest.4ba70bb9a67b351962916ae4d02e5961.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8da3b24129060ecd094f91b8849cb5",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "158dd085a09ac145d45e",
+    "url": "/ReactWeather/static/css/main.edd90992.chunk.css"
+  },
+  {
+    "revision": "6e9b39b15c1315a55664",
+    "url": "/ReactWeather/static/js/2.a79258a7.chunk.js"
+  },
+  {
+    "revision": "5353a3cc2cc914ae668f7596a4530e6a",
+    "url": "/ReactWeather/static/js/2.a79258a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158dd085a09ac145d45e",
+    "url": "/ReactWeather/static/js/main.829fa836.chunk.js"
+  },
+  {
+    "revision": "bec2545d28062512884c",
+    "url": "/ReactWeather/static/js/runtime-main.31b1d042.js"
+  },
+  {
+    "revision": "6eb21da79303a35df1750607558ebeae",
+    "url": "/ReactWeather/static/media/bg.6eb21da7.svg"
+  },
+  {
+    "revision": "47f1e6bdd7af84a534875c40ed81aac6",
+    "url": "/ReactWeather/static/media/cloudy.47f1e6bd.svg"
+  },
+  {
+    "revision": "2ae50b330e08793b35ee4a5e729df69e",
+    "url": "/ReactWeather/static/media/darkBg.2ae50b33.svg"
+  },
+  {
+    "revision": "5f81c3a6815e7834163e83875312feb8",
+    "url": "/ReactWeather/static/media/github.5f81c3a6.svg"
+  },
+  {
+    "revision": "0f8ede749e5dc61255d557c6ffaf9a46",
+    "url": "/ReactWeather/static/media/haze.0f8ede74.svg"
+  },
+  {
+    "revision": "17e40bd5c94c2cbedac31d7d7de613d1",
+    "url": "/ReactWeather/static/media/heavy-rain.17e40bd5.svg"
+  },
+  {
+    "revision": "931f8d0e10c490b2770cd939a3f68802",
+    "url": "/ReactWeather/static/media/high-icon.931f8d0e.svg"
+  },
+  {
+    "revision": "43a903ed0bfd53764b61806544b02860",
+    "url": "/ReactWeather/static/media/humidity-icon.43a903ed.svg"
+  },
+  {
+    "revision": "948b91fd4fa54ebd87a2082571678dfd",
+    "url": "/ReactWeather/static/media/location-icon.948b91fd.svg"
+  },
+  {
+    "revision": "4cadc629aa2ffd7dabafb0df86b890fb",
+    "url": "/ReactWeather/static/media/low-icon.4cadc629.svg"
+  },
+  {
+    "revision": "c2e06f0212a9f4db46120dcf7ae2cc1e",
+    "url": "/ReactWeather/static/media/partly-cloudy.c2e06f02.svg"
+  },
+  {
+    "revision": "2284050a3cba90029fd95ab123b2b0b8",
+    "url": "/ReactWeather/static/media/pressure-icon.2284050a.svg"
+  },
+  {
+    "revision": "0138fd80e586127094a2058ae6cfd5d7",
+    "url": "/ReactWeather/static/media/rain.0138fd80.svg"
+  },
+  {
+    "revision": "aff8f2a35e11e2c00e548e3c6c2f5207",
+    "url": "/ReactWeather/static/media/search-icon.aff8f2a3.svg"
+  },
+  {
+    "revision": "4a7dd48bb6842dfd0f5d6f4d339ac651",
+    "url": "/ReactWeather/static/media/sleet.4a7dd48b.svg"
+  },
+  {
+    "revision": "f6477891c68a906fcc6091c69b8adc4d",
+    "url": "/ReactWeather/static/media/snow.f6477891.svg"
+  },
+  {
+    "revision": "3402daa5b40fff8cc27934f5b4fe9f6d",
+    "url": "/ReactWeather/static/media/sunny.3402daa5.svg"
+  },
+  {
+    "revision": "d0dcc540816c0878adc943b4fcb54ba9",
+    "url": "/ReactWeather/static/media/thunderstorm.d0dcc540.svg"
+  },
+  {
+    "revision": "7004d253811665b97b8f072088ae6de9",
+    "url": "/ReactWeather/static/media/wind-icon.7004d253.svg"
+  }
+]);
